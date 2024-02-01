e6530154480006945a68fa43c61d1f99136eab8c
starishsky <117216668+starishsky@users.noreply.github.com>
2024-02-01 05:45:06 -0500

    Update sketch.js

--- assignments/p5/01_abstract/sketch.js
+++ assignments/p5/01_abstract/sketch.js
@@ -10,21 +10,12 @@ function setup() {
  // put setup code here
 
   createCanvas(1136, 852);
-  smallPoint = 4;
-  largePoint = 20;
-  imageMode(CENTER);
-  noStroke();
+
   background(255);
-  ricardo.loadPixels();
 }
 
 function draw() {
   // put drawing code here
 
-  let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
-  let y = floor(random(ricardo.height));
-  let x = floor(random(ricardo.width));
-  let pix = ricardo.get(x, y);
-  fill(pix, 128);
-  ellipse(x, y, pointillize, pointillize);
+
 }
