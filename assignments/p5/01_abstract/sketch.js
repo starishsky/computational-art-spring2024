85041f57838ded840b93e64df8d4f717f408950a
starishsky <117216668+starishsky@users.noreply.github.com>
2024-02-01 05:37:59 -0500

    god i hope this works

--- assignments/p5/01_abstract/sketch.js
+++ assignments/p5/01_abstract/sketch.js
@@ -1,7 +1,30 @@
+let ricardo;
+
+let smallPoint, largePoint;
+
+function preload () {
+  ricardo = loadImage('ricardo.jpg');
+}
+
 function setup() {
  // put setup code here
+
+  createCanvas(1136, 852);
+  smallPoint = 4;
+  largePoint = 20;
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  ricardo.loadPixels();
 }
 
 function draw() {
   // put drawing code here
+
+  let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  let y = floor(random(ricardo.height));
+  let x = floor(random(ricardo.width));
+  let pix = ricardo.get(x, y);
+  fill(pix, 128);
+  ellipse(x, y, pointillize, pointillize);
 }
