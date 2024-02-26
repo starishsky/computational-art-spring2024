0d7db7e603ee0b70c01b7c67e951c19315171989
starishsky <117216668+starishsky@users.noreply.github.com>
2024-02-26 06:02:12 -0500

    jesus help

--- assignments/04_particles/sketch.js
+++ assignments/04_particles/sketch.js
@@ -1,19 +1,21 @@
 let emitter;
 let img;
 
+
 function preload(){
     img = loadImage('howl.png');
 }
 
 function setup(){
     createCanvas(1920, 1040);
-    image(img,0,0);
+    
     emitter = new Emitter(width / 2, height / 2);
 }
 
 function draw(){
-    
-    let force = createVector(0, 0.05);
+    image(img,0,0);
+    // background(0);
+    let force = createVector(0, 0.03);
     emitter.applyForce(force);
     emitter.emit(1);
     emitter.update();
