d4b3f5daa90b9477c9f5806b27979ba74c1485ba
starishsky <117216668+starishsky@users.noreply.github.com>
2024-02-29 02:06:16 -0500

    holy moly i did it i have clown circles

---
+++ assignments/05_grid/sketch.js
@@ -0,0 +1,40 @@
+let gridSize = 10;
+let numCols, numRows;
+let noiseValue;
+let diameter;
+
+let colorGrid = [];
+
+
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background(220);
+
+  numCols = width / gridSize;
+  numRows = height / gridSize;
+  
+  for (let col = 0; col < numCols; col++) {
+    for (let row = 0; row < numRows; row++) {
+      const x = col * gridSize;
+      const y = row * gridSize;
+
+      //constants are like lets but they aren't reassigned
+      
+      noiseValue = noise(col * 0.1, row * 0.1);
+      
+      diameter = gridSize * (1 + sin(noiseValue * TWO_PI));
+      //2pi is pi2 and used for sine to be twice the ratio of circ to dia
+      
+      ellipse(x, y, diameter, diameter);
+      noStroke();
+      
+
+      fill(random(255), random(255), random(255));
+    }
+  }
+}
+
